35cef177f8eb0d79ee190c4360d0f67461b56ef0
abubakar83 <abubakar.abs83@gmail.com>
2025-09-16 08:32:42 +0500

    Fix import case sensitivity for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header/Header";
-import Hero from "./components/Hero/hero";
+import Hero from "./components/Hero/Hero";
 import "../App.css"
 import Companies from "./components/companies/Companies";
 import Residencies from "./Residencies/Residencies";
